ba4d334e59174b8af1dc248f4e7da528e57ff6cd
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-23 20:25:15 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
+const homeController = require("../controller/home_controller");
 
 console.log("Router loaded");
 
+router.get("/", homeController.home);
 module.exports = router;
