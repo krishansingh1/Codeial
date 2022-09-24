ae1f38cadbf2767b45bd93d40c535aa232e72126
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-24 18:16:57 +0530

    Commit

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,9 @@
 module.exports.home = function (req, res) {
-  return res.end("<h1>Express is ready and setup</h1>");
+  //return res.end("<h1>Express is ready and setup</h1>");
+
+  return res.render("home", {
+    title: "Home",
+  });
 };
 
 module.exports.home2 = function (req, res) {
