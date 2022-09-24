e3712a7746eac17a53c675e0ada6da70f72b91c9
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-24 18:02:43 +0530

    Commit

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function (req, res) {
   return res.end("<h1>User Controller is up and runinn!</h1>");
 };
+
+module.exports.profile2 = function (req, res) {
+  return res.end("<h1>Profile 2 Controller is up and runinn!</h1>");
+};
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userController = require("../controller/users_controller");
 
 router.get("/profile", userController.profile);
+router.get("/profile2", userController.profile2);
 
 module.exports = router;
