e1da67406abdc8ea2d4755252ae81ab972cc964e
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-09 00:10:57 +0530

    Commit

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,3 +1,6 @@
 module.exports.posts = function (req, res) {
-  return res.end("<h1>Posts controller is up and runnin!</h1>");
+  return res.render("post", {
+    title: "Posts",
+    post: "Post is up and running!",
+  });
 };
