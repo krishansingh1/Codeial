4e9366bfaa3168a3eb3de20be581660b077fc805
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-09 20:42:19 +0530

    Commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 app.use(express.static("./assets"));
 app.use(expressLayouts);
 
+//extract styles and scripts from sub page into the layout
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.use("/", require("./routes"));
 
 app.set("view engine", "ejs");
