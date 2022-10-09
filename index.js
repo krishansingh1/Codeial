3d9ece1e8ef169d3783ddb06404d218075aa0190
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-09 19:53:23 +0530

    Commit

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 const port = 8000;
 
 app.use("/", require("./routes"));
-app.use(expressLayouts);
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(expressLayouts);
+
 app.listen(port, (err) => {
   if (err) {
     console.log(`Error:${err}`);
