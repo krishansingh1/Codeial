e177b9f0c2d4e29dc3de8dbbf7b4e1cf741566b4
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-13 11:27:47 +0530

    Commit

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,7 +1,14 @@
 module.exports.signUp = function (req, res) {
-  return res.end("<h1>User Controller is up and runinn!</h1>");
+  // return res.end("<h1>User Controller is up and runinn!</h1>");
+
+  return res.render("signUp", {
+    title: "Codeial/ SignUp",
+  });
 };
 
 module.exports.signIn = function (req, res) {
-  return res.end("<h1>Profile 2 Controller is up and runinn!</h1>");
+  // return res.end("<h1>Profile 2 Controller is up and runinn!</h1>");
+  return res.render("signIn", {
+    title: "Codeial / SignIn",
+  });
 };
