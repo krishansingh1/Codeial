07913f73e38e1cc65240e4037b84135d630b8b35
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-14 19:08:24 +0530

    Commit

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -16,29 +16,24 @@ module.exports.signIn = function (req, res) {
 };
 
 module.exports.create = function (req, res) {
-  if (req.body.Password != req.body.Confirm_password) {
+  if (req.body.password != req.body.confirm_password) {
     return res.redirect("back");
   }
 
-  User.findOne({ email: req.body.Email }, function (err, user) {
+  User.findOne({ email: req.body.email }, function (err, user) {
     if (err) {
       console.log("Error in finding user in signing up");
       return;
     }
 
     if (!user) {
-      User.create(
-        req.body.Email,
-        req.body.Password,
-        req.body.Name,
-        function (err, user) {
-          if (err) {
-            console.log("Error in creating user while signing up");
-            return;
-          }
-          return res.redirect("/users/signIn");
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error in creating user while signing up");
+          return;
         }
-      );
+        return res.redirect("/users/signIn");
+      });
     } else {
       return res.redirect("back");
     }
