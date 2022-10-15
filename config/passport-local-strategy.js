a52e49191a2a2eeed6b44e20acf49a9c39b0cf27
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-15 19:05:04 +0530

    Commit

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,14 +1,31 @@
 const passport = require("passport");
 
+const User = require("../models/user");
+
 const LocalStrategy = require("passport-local").Strategy;
 
+//authentication using passport
 passport.use(
   new LocalStrategy(
     {
       usernameField: "email",
     },
     function (email, password, done) {
-      //find a user establish the identity
+      //find a user and establish the identity
+      User.findOne({ email: email }, function (err, user) {
+        if (err) {
+          console.log("Error in finding user ---> Passport");
+          return done(err);
+        }
+
+        if (!user || user.password != password) {
+          console.log("Invalid UserName/Password");
+
+          return done(null, false);
+        }
+
+        return done(null, user);
+      });
     }
   )
 );
