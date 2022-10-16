23475e02852f3d510c5e66ed7d8f88fb33916b26
Krishan Kumar <kkrishan7074@gmail.com>
2022-10-16 19:07:30 +0530

    Commit

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,11 @@ router.get("/profile", userController.profile);
 router.get("/signUp", userController.signUp);
 router.get("/signIn", userController.signIn);
 router.post("/create", userController.create);
-router.post("/create-session",passport, userController.createSession);
+router.post(
+  "/create-session",
+  passport.authenticate("local", { failureRedirect: "/users/signIn" }),
+  userController.createSession
+);
 router.get("/delete", userController.delete);
 
 module.exports = router;
