c0a9eb604d0a8ff8fa1f01f507f3ea4aead36099
Krishan Kumar <kkrishan7074@gmail.com>
2022-11-02 17:50:35 +0530

    Commit

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -24,3 +24,23 @@ module.exports.create = function (req, res) {
     }
   });
 };
+
+module.exports.destroy = function (req, res) {
+  Comment.findById(req.params.id, function (err, comment) {
+    if (comment.user == req.user.id) {
+      let postId = comment.user;
+
+      comment.remove();
+
+      Post.findByIdAndUpdate(
+        postId,
+        { $pull: { comments: req.params.id } },
+        function (err, post) {
+          return res.redirect("back");
+        }
+      );
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
