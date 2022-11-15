8884ab43349fe166a32d37215f7a33e0febc9ba1
Krishan Kumar <kkrishan7074@gmail.com>
2022-11-15 12:43:48 +0530

    Commit

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -25,7 +25,15 @@ module.exports.update = async function (req, res) {
         if (err) {
           console.log("*****Multer Error", err);
         }
-        console.log(req.file);
+        user.name = req.body.name;
+        user.email = req.body.email;
+
+        if (req.file) {
+          //this is savig the path of the uploaded file into the avatar field in the user
+          user.avatar = User.avatarPath + "/" + req.file.filename;
+        }
+        user.save();
+        return res.redirect("back");
       });
     } catch (err) {
       req.flash("error", err);
