f947ef2e8391f6a82476e49515326783c4368932
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-12 19:52:43 +0530

    Commit

---
+++ controller/api/v1/users_api.js
@@ -0,0 +1,26 @@
+const User = require("../../../models/user");
+
+const JWT = require("jsonwebtoken");
+
+module.exports.createSession = async function (req, res) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (!user || user.password != req.body.password) {
+      return res.json(422, {
+        message: "Invalid username or password",
+      });
+    }
+
+    return res.json(200, {
+      message: "Sign in successful, here is you token, please keep it safe!",
+      data: {
+        token: JWT.sign(user.toJSON(), "codeial", { expiresIn: "100000" }),
+      },
+    });
+  } catch (err) {
+    return res.json("500", {
+      message: "Internal Server Error",
+    });
+  }
+};
