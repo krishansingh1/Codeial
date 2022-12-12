18cecc45b972e93458a7ee6118d679256ca8f45a
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-12 17:05:12 +0530

    Nodemailer file added for mails

---
+++ config/nodemailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const path = require("path");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "kkrishan7074",
+    pass: "yomxdgwdwahutvtd",
+  },
+});
+
+let renderTemplate = (data, relativePath) => {
+  let mailHTML;
+  ejs.renderFile(
+    path.join(__dirname, "../views/mailers", relativePath),
+    data,
+    function (err, template) {
+      if (err) {
+        console.log("Error in rendering template", err);
+        return;
+      }
+
+      mailHTML = template;
+    }
+  );
+
+  return mailHTML;
+};
+
+module.exports = {
+  transporter: transporter,
+  renderTemplate: renderTemplate,
+};
