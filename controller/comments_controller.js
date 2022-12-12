aa0c6e5e0599908a8d9295dfdf1bf5849e9678b9
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-12 19:06:58 +0530

    Commit

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,47 +1,92 @@
-const Comment = require("../models/comment");
-const Post = require("../models/post");
+const Post = require('../models/post_schema');
+const Comment = require('../models/comment_schema');
+const Like = require('../models/like_schema');
+//const queue = require('../config/kue');
+//const commentWorker = require('../worker/comment_worker');
+const commentMailer = require('../mailers/comment_mailer');
 
-module.exports.create = async function (req, res) {
-  try {
-    let post = await Post.findById(req.body.post);
 
-    if (post) {
-      let comment = await Comment.create({
-        content: req.body.content,
-        post: req.body.post,
-        user: req.user._id,
-      });
+module.exports.create = async function(req,res){
+    try{
+        let post = await Post.findById(req.body.post);
+        if(post){
+            let comments = await Comment.create(
+                {
+                    content: req.body.content,
+                    post : req.body.post,
+                    user: req.user._id
+                }
+            );
+            post.comment.push(comments);
+            post.save();
+           
+            comments = await comments.populate('user', 'first email');
+            /*queue.create('emails', comments).save(function(err){
+                console.log('err',err);
+                return;
+            });*/
+            commentMailer.newComment(comments);    
+            if(req.xhr){
+                return res.status(200).json({
+                    data : {
+                        post: post,
+                        comment : comments,
+                        user : req.user
+                    },
+                    message: 'coment created!'
+                });
+            }
+            req.flash('success', 'Comment posted successfully');
+            return res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
 
-      post.comments.push(comment);
-      post.save();
-
-      return res.redirect("/");
+    }catch(err){
+        console.log('error', err);
     }
-  } catch (err) {
-    console.log("Error in adding the comments", err);
-    return;
-  }
-};
+}
 
-module.exports.destroy = async function (req, res) {
-  try {
+
+
+module.exports.destroy = async function(req,res){
+   try{
     let comment = await Comment.findById(req.params.id);
+    if(comment.user == req.user.id){
+        let postId = comment.post;
+        comment.remove();
+        let post = await Post.findByIdAndUpdate(postId, { $pull : {comment: req.params.id}});
+        await Like.deleteMany({likeable : comment._id, onModel: 'Comment'});
 
-    if (comment.user == req.user.id) {
-      let postId = comment.user;
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    comment_id : req.params.id
+                },
+                message: 'comment deleted'
+            });
+        }
+        req.flash('error', 'Comment deleted successfully');
+        return res.redirect('back');
+    }else{
+        let postId = comment.post;
+        comment.remove();
+        await Post.findByIdAndUpdate(postId, { $pull : {comment: req.params.id}});
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    comment_id : req.params.id
+                },
+                message: 'comment deleted'
+            });
+        }
+        req.flash('error', 'Comment deleted successfully');
+        return res.redirect('back');
+    
 
-      comment.remove();
-
-      await Post.findByIdAndUpdate(postId, {
-        $pull: { comments: req.params.id },
-      });
-
-      return res.redirect("back");
-    } else {
-      return res.redirect("back");
     }
-  } catch (err) {
-    console.log("Error", err);
-    return;
-  }
-};
+   }catch(err){
+    console.log(err);
+   }
+    
+}
