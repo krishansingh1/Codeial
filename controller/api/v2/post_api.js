c2f610fef166f31b299b1ffcb05ca6965238365e
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-15 13:29:53 +0530

    Changes and Commit

--- controller/api/v2/post_api.js
+++
@@ -1,5 +0,0 @@
-module.exports.index = function(req,res){
-    return res.json(200, {
-        message: 'post api 2 created'
-    });
-}
--- controller/home_controller.js
+++ controller/home_controller.js
@@ -2,16 +2,6 @@ const Post = require("../models/post");
 const User = require("../models/user");
 
 module.exports.home = async function (req, res) {
-  // console.log(req.cookies);
-  // res.cookie("user_id", 25);
-
-  // Post.find({}, function (err, posts) {
-  //   return res.render("home", {
-  //     title: "Home Page",
-  //     posts: posts,
-  //   });
-  // });
-
   try {
     let posts = await Post.find({})
       .sort("-createdAt")
