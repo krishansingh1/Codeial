8dd0fec2a361ca0dc72d5f1383ac81a67eaf7d74
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 11:34:00 +0530

    Commit

--- assets/js/home_comment.js
+++ assets/js/home_comment.js
@@ -35,6 +35,9 @@ class PostComments {
           $(`#post-comments-${postId}`).prepend(newComment);
           pSelf.deleteComment($(" .delete-comment-button", newComment));
 
+          // CHANGE :: enable the functionality of the toggle like button on the new comment
+          new ToggleLike($(' .toggle-like-button', newComment));
+
           new Noty({
             theme: "relax",
             text: "Comment published!",
@@ -61,7 +64,11 @@ class PostComments {
                   <i class="fa-solid fa-trash-can"></i>
                 </a>
               </span>
-            <% } %>
+              <small>          
+                <a class="toggle-like-button" data-likes="0" href="/likes/toggle/?id=${comment._id}&type=Comment">
+                  0 Likes
+                </a>
+              </small>
             </li>`);
   }
 
