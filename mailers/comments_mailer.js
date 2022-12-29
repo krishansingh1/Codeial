11418d4e3f2e405316168ebed3b71ffd7aeab2d0
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 11:58:25 +0530

    Commit

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,26 +1,26 @@
 const nodemailer = require("../config/nodemailer");
 
 exports.newComment = (comment) => {
-    let htmlString = nodemailer.renderTemplate(
-        { comment: comment },
-        "/comments/new_comment.ejs"
-    );
+  let htmlString = nodemailer.renderTemplate(
+    { comment: comment },
+    "/comments/new_comment.ejs"
+  );
 
-    nodemailer.transporter.sendMail(
-        {
-            from: "codeial.com",
-            to: comment.user.email,
-            subject: "New comment Published!",
-            html: htmlString,
-        },
-        (err, info) => {
-            if (err) {
-                console.log("Error in sending mail,", err);
-                return;
-            }
+  nodemailer.transporter.sendMail(
+    {
+      from: "codeial.com",
+      to: comment.user.email,
+      subject: "New comment Published!",
+      html: htmlString,
+    },
+    (err, info) => {
+      if (err) {
+        console.log("Error in sending mail,", err);
+        return;
+      }
 
-            // console.log("Mail sent", info);
-            return;
-        }
-    );
+      // console.log("Mail sent", info);
+      return;
+    }
+  );
 };
