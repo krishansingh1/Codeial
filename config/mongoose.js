3f8ae94fb0115948e0dda016207889f943d2b905
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 11:50:07 +0530

    Commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/codeial");
+const env = require('./environment');
+mongoose.connect(`mongodb://localhost:27017/${env.db}`);
 
 const db = mongoose.connection;
 
