bcac5f4cc2f732a7925ae18aa490cd90b63c2c09
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 12:09:16 +0530

    Commit

---
+++ routes/add.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const addController = require('../controller/add_controller');
+
+router.post('/add_toggle', addController.addToggle);
+
+module.exports = router;
