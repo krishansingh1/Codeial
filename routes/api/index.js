ba54b8385a3f63e2761cb3c7d50a582f43cb23a6
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 19:06:45 +0530

    Commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/posts", require("./posts"));
-router.use("/users", require("./users"));
+router.use("/v1", require("./v1"));
 
 module.exports = router;
