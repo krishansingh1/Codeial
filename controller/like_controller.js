4426ef11db202f4bc0ddce66d8a17209626d9cd1
Krishan Kumar <49793696+krishansingh1@users.noreply.github.com>
2022-12-29 11:55:33 +0530

    Commit

--- controller/like_controller.js
+++ controller/like_controller.js
@@ -1,54 +1,64 @@
-const Post = require('../models/post_schema');
-
+const Like = require("../models/like_schema");
+const Post = require("../models/post_schema");
 const Comment = require('../models/comment_schema');
-
-const Like = require('../models/like_schema');
 
 module.exports.toggleLike = async function (req, res) {
     try {
-        let liked;
+
+        // likes/toggle/?id=abcdef&type=Post
+        let likeable;
         let deleted = false;
 
-        if (req.query.type == "Post") {
-            liked = await Post.findById(req.query.id).populate('likes');
+
+        if (req.query.type == 'Post') {
+            likeable = await Post.findById(req.query.id).populate('likes');
         } else {
-            liked = await Comment.findById(req.query.id).populate('likes');
+            likeable = await Comment.findById(req.query.id).populate('likes');
         }
 
-        let exitingLike = await Like.findOne({
-            user: req.user._id,
+
+        // check if a like already exists
+        let existingLike = await Like.findOne({
             likeable: req.query.id,
             onModel: req.query.type,
-        });
+            user: req.user._id
+        })
 
-        if (exitingLike) {
-            liked.likes.pull(exitingLike._id);
-            liked.save();
-            exitingLike.remove();
+        // if a like already exists then delete it
+        if (existingLike) {
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+
+            existingLike.remove();
             deleted = true;
+
         } else {
+            // else make a new like
+
             let newLike = await Like.create({
                 user: req.user._id,
                 likeable: req.query.id,
-                onModel: req.query.type,
+                onModel: req.query.type
             });
-            console.log('before', liked);
-            liked.likes.push(newLike._id);
-            console.log('after', liked.likes[0]);
-            liked.save();
+
+            likeable.likes.push(newLike._id);
+            likeable.save();
+
         }
 
-        return res.status(200).json({
-            message: 'success',
+        return res.json(200, {
+            message: "Request successful!",
             data: {
-                deleted: deleted,
+                deleted: deleted
             }
         })
 
+
+
     } catch (err) {
         console.log(err);
-        return res.status(500).json({
-            message: err,
-        })
+        return res.json(500, {
+            message: 'Internal Server Error'
+        });
     }
 }
